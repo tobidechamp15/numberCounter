d5058093bdbbcef9de068cf613c6aacb69b5069b
unknown <tobidechamp15@gmail.com>
2023-06-29 14:50:17 +0100

    initial-commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const sub = document.querySelector("#minus");
+const num=document.querySelector("#number");
+const plus = document.querySelector("#add")
+
+sub.addEventListener("click", reduceNumber)
+
+function reduceNumber() {
+  // Get the current number value from the HTML element
+  let number = parseInt(document.getElementById("numberDisplay").textContent);
+
+  // Reduce the number by 1
+  number--;
+
+  // Update the HTML element with the new number value
+  document.getElementById("numberDisplay").textContent = number;
+}
+
+plus.addEventListener("click", increaseNumber);
+
+function increaseNumber() {
+  // Get the current number value from the HTML element
+  let number = parseInt(document.getElementById("numberDisplay").textContent);
+
+  // Reduce the number by 1
+  number++;
+
+  // Update the HTML element with the new number value
+  document.getElementById("numberDisplay").textContent = number;
+}
